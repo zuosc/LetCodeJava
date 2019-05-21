1768bd6eed8d153a53e81e826cf8d6613aa01584
zuosc <zorro.zsc@hotmail.com>
2019-05-21 14:17:36 +0800

    449

--- src/com/z/learn/_449serializeAndDeserializeBst/SerializeAndDeserializeBst.java
+++ src/com/z/learn/_449serializeAndDeserializeBst/SerializeAndDeserializeBst.java
@@ -1,8 +1,80 @@
 package com.z.learn._449serializeAndDeserializeBst;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class SerializeAndDeserializeBst {
+    private static final String spliter = ",";
+    private static final String NN = "#";
+
     public static void main(String[] args) {
 
+    }
+
+    private String serialize1(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.poll();
+            if (t != null) {
+                sb.append(String.valueOf(t.val)).append(",");
+                queue.add(t.left);
+                queue.add(t.right);
+            } else {
+                sb.append("#,");
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+
+    private String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+
+    private void buildString(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append(NN).append(spliter);
+        } else {
+            sb.append(node.val).append(spliter);
+            buildString(node.left, sb);
+            buildString(node.right, sb);
+        }
+    }
+
+
+    private TreeNode deserialize(String data) {
+        Deque<String> nodes = new LinkedList<>(Arrays.asList(data.split(spliter)));
+        return buildTree(nodes);
+    }
+    private TreeNode buildTree(Deque<String> nodes) {
+        String val = nodes.remove();
+        if (val.equals(NN)) return null;
+        else {
+            TreeNode node = new TreeNode(Integer.valueOf(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    //左孩子
+    TreeNode left;
+    //右孩子
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
     }
 }
